7f81c4ae4e90b96dca8711d40a846ab35fad7a33
Matt Furman <furman82@gmail.com>
2013-08-22 16:14:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FPBL::Application.config.secret_token = 'a101dd5012bacc93b86fada3a1c04b6b6bb4e203a5f05b47ba848f5550eb21f03e8aafd4703d40cd86c92f87abc1038f6d4ab58e59289ba50a9c03b0ac175eb9'
