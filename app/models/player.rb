919694d5924ef5b6a9e4cae55d878924d4c0b8a3
David Furstein <david.furstein@gmail.com>
2013-11-13 21:02:41 -0500

    Reloaded DB; Switched to Bootstrap 3; Added panels to standings page

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ActiveRecord::Base
   attr_accessible :dmb_id, :first_name, :last_name, :position
 
   has_many :contracts
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
