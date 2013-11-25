d02719d4672c45909c1ca42c1aa526527149002f
David Furstein <david.furstein@gmail.com>
2013-11-24 22:34:06 -0500

    Added functions to get team objects for each game. Could it be done a better way via the model setup?

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,19 @@
 class Schedule < ActiveRecord::Base
   attr_accessible :away_score, :away_team, :date, :extra_innings, :home_score, :home_team
+
+  def home
+    Season.where("year = #{self.date.year}").collect { |season| 
+      season.team
+    }.find { |team| 
+      team.abbreviation.upcase == self.home_team.upcase 
+    }
+  end
+
+  def away
+    Season.where("year = #{self.date.year}").collect { |season| 
+      season.team
+    }.find { |team| 
+      team.abbreviation.upcase == self.away_team.upcase 
+    }
+  end
 end
