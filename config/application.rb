3ad2869619a03dba51feeb0a5765f998541705b1
David Furstein <david.furstein@gmail.com>
2014-03-25 17:54:57 -0400

    Monke patching the String class

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module FPBL
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    #config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
