c7cd8c282b46df1f9582a67a9fe3a1743271c036
David Furstein <david.furstein@gmail.com>
2014-08-03 22:07:50 -0400

    Updating schema (part 2)

---
+++ db/migrate/20131029181943_create_standings.rb
@@ -0,0 +1,20 @@
+# Migration to create team record
+class CreateStandings < ActiveRecord::Migration
+  def change
+    create_table :standings, id: false do |t|
+      t.integer :year, null: false
+      t.integer :franchise_id, null: false
+      t.string :league, null: false
+      t.string :division, null: false
+      t.integer :wins, null: false
+      t.integer :losses, null: false
+      t.integer :streak, null: false
+      t.string :playoff_berth
+      t.string :playoff_depth
+
+      t.timestamps
+    end
+
+    add_index :standings, [:year, :franchise_id], unique: true
+  end
+end
---
+++ db/migrate/20140324211205_create_boxscores.rb
@@ -0,0 +1,47 @@
+# Migration to create boxscores
+class CreateBoxscores < ActiveRecord::Migration
+  def change
+    create_table :boxscores, id: false do |t|
+      t.date :date, null: false
+      t.string :dmb_name, null: false
+      t.string :team, null: false
+      t.string :position, null: false
+      t.string :played_against, null: false
+      t.boolean :is_home_team, null: false
+      t.integer :at_bat, default: 0
+      t.integer :run, default: 0
+      t.integer :hit, default: 0
+      t.integer :run_batted_in, default: 0
+      t.integer :double, default: 0
+      t.integer :triple, default: 0
+      t.integer :homerun, default: 0
+      t.integer :steal, default: 0
+      t.integer :caught_stealing, default: 0
+      t.integer :strike_out, default: 0
+      t.integer :walk, default: 0
+      t.integer :sacrifice_fly, default: 0
+      t.integer :sacrifice, default: 0
+      t.integer :hit_by_pitch, default: 0
+      t.integer :win, default: 0
+      t.integer :loss, default: 0
+      t.integer :hold, default: 0
+      t.integer :save_game, default: 0
+      t.integer :blown_save, default: 0
+      t.decimal :inning, default: 0
+      t.integer :allowed_hit, default: 0
+      t.integer :allowed_run, default: 0
+      t.integer :allowed_earned_run, default: 0
+      t.integer :allowed_walk, default: 0
+      t.integer :allowed_strike_out, default: 0
+      t.integer :hit_batter, default: 0
+      t.integer :wild_pitch, default: 0
+      t.integer :passed_ball, default: 0
+      t.integer :balk, default: 0
+      t.integer :error, default: 0
+
+      t.timestamps
+    end
+
+    add_index :boxscores, [:date, :dmb_name], unique: true
+  end
+end
