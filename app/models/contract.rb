dd0ed3f897793641f131f672caf3ee50ed673793
David Furstein <david.furstein@gmail.com>
2014-08-15 11:42:59 -0400

    Adding secondary info to contracts view

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -17,4 +17,34 @@ class Contract < ActiveRecord::Base
     Contract.under_contract(year, franchise_id)
       .select { |player| player.position.upcase == position.upcase }
   end
+
+  def self.total_salary(year, franchise_id)
+    Contract.where(year: year, franchise_id: franchise_id)
+      .map { |contract| contract.salary.round(1) }.sum
+  end
+
+  def self.salary_cap(year, franchise_id)
+    Team.find(year, franchise_id).salary_cap
+  end
+
+  def self.available_salary_cap(year, franchise_id)
+    salary_cap(year, franchise_id) - total_salary(year, franchise_id)
+  end
+
+  def self.total_active_players(year, franchise_id)
+    players = Contract.where(year: year, franchise_id: franchise_id,
+                             released: false)
+      .map { |contract| contract.player }
+
+    if year == Team.last.year
+      players.select { |player| player.active }.count
+    else
+      players.count
+    end
+  end
+
+  def self.total_inactive_players(year, franchise_id)
+    Contract.where(year: year, franchise_id: franchise_id).count -
+      total_active_players(year, franchise_id)
+  end
 end
