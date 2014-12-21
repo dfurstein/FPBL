2073c539b0cc6f3f0be65c7427b3020ff5041dc2
David Furstein <david.furstein@gmail.com>
2014-12-21 16:32:27 -0500

    add method to add new owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,6 +4,16 @@ class Owner < ActiveRecord::Base
 
   has_many :teams
 
+  def self.add_owner(first_name, last_name, email)
+    owner = Owner.new
+
+    owner.first_name = first_name
+    owner.last_name = last_name
+    owner.email = email
+
+    owner.save
+  end
+
   def name
     first_name + ' ' + last_name
   end
