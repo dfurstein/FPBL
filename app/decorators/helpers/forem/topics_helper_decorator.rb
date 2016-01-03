283579f41f63cd9f57f17691f542a4eb6d5f5d15
David Furstein <david.furstein@gmail.com>
2016-01-03 01:26:52 -0500

    Updated to show last edit date.

---
+++ app/decorators/helpers/forem/topics_helper_decorator.rb
@@ -0,0 +1,20 @@
+Forem::TopicsHelper.class_eval do
+  def link_to_latest_post(topic)
+    post = relevant_posts(topic).last
+    text = "#{time_ago_in_words(post.updated_at)} #{t("ago_by")} #{post.user.forem_name}"
+    link_to text, forem.forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}", pagination_param => topic.last_page)
+  end
+
+  def new_since_last_view_text(topic)
+    if forem_user
+      topic_view = topic.view_for(forem_user)
+      forum_view = topic.forum.view_for(forem_user)
+
+      if forum_view
+        if topic_view.nil? && topic.updated_at > forum_view.past_viewed_at
+          "<span class='glyphicon glyphicon-star' aria-hidden='true'></span>".html_safe
+        end
+      end
+    end
+  end
+end
