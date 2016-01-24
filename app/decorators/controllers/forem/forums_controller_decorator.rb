affcba4f580ea5c253d200311e190680741db148
David Furstein <david.furstein@gmail.com>
2016-01-23 22:36:23 -0500

    Pinned topics stay at the top.

--- app/decorators/controllers/forem/forums_controller_decorator.rb
+++ app/decorators/controllers/forem/forums_controller_decorator.rb
@@ -8,7 +8,7 @@ Forem::ForumsController.class_eval do
       @forum.topics.visible.approved_or_pending_review_for(forem_user)
     end
 
-    @topics = @topics.sort_by { |topic| topic.posts.max_by { |post| post.updated_at }.updated_at }.reverse!
+    @topics = @topics.sort_by { |topic| [topic.pinned ? 1 : 0, topic.posts.max_by { |post| post.updated_at }.updated_at] }.reverse!
 
     # Kaminari allows to configure the method and param used
     @topics = Kaminari.paginate_array(@topics).page(params[pagination_param]).per(Forem.per_page)
